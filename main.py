a82fb769420e546604671ad87f2f0651373c6df0
Zack Scriven <zackscriven@gmail.com>
2025-01-13 13:31:26 -0700

    added .env

--- main.py
+++ main.py
@@ -15,3 +15,30 @@ if __name__ == '__main__':
     print_hi('mes_core')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Start of Test Query Code -- retrieve record from enterprise table in mes_core schema
+# Connect to MySQL db
+
+print("Start Test")
+
+import mysql.connector
+
+# Establishing the connection
+cnx = mysql.connector.connect(
+    user='Root',
+    password='MySQLadmin42',
+    host='127.0.0.1',
+    database='mes_core'
+)
+
+try:
+    # Creating a cursor to execute queries
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM enterprise")
+    
+    # Fetching all results from the query
+    result = cursor.fetchall()
+    print(result)
+    
+finally:
+    # Closing the connection
+    cnx.close()
