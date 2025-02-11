d79bd0cfd6ac5408fe1b7edb80dd00cb37215d81
Zack Scriven <zackscriven@gmail.com>
2025-02-10 20:40:12 -0700

    alter_tables.sql

--- Module 6/alter_tables.sql
+++ Module 6/alter_tables.sql
@@ -3,3 +3,9 @@
 -- run this code against your mes_core database
 ALTER TABLE `mes_core`.`workorder` 
 CHANGE COLUMN `ProductCodeID` `ProductCodeID` INT NULL ;
+
+
+
+-- alter the schedule table ScheduleType datatype to VARCHAR(45)
+ALTER TABLE `mes_core`.`schedule` 
+CHANGE COLUMN `ScheduleType` `ScheduleType` VARCHAR(45) NOT NULL ;
